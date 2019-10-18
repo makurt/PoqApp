3c58d610b1e9829b712e72e37abc13c2ef4fb7cb
Daniel Eusebio Benito Esteban <daniel.benito@poqcommerce.com>
2019-10-18 11:26:45 +0100

    Add injection annotation by default and gradle property to prevent heap problems with processing

--- Poq.Android.TemplateClient/app/src/main/java/com/poqstudio/app/client/TemplateClientApplication.kt
+++ Poq.Android.TemplateClient/app/src/main/java/com/poqstudio/app/client/TemplateClientApplication.kt
@@ -1,5 +1,27 @@
 package com.poqstudio.app.client
 
 import com.poqstudio.app.platform.PoqApplication
+import com.poqstudio.app.platform.PoqApplicationComponent
+import com.poqstudio.app.platform.dagger.component.ActivityBuilder
+import com.poqstudio.app.platform.injection.annotations.PoqComponent
 
-class TemplateClientApplication : PoqApplication()
+@PoqComponent(
+    extraModules = [
+        ActivityBuilder::class
+    ]
+) class TemplateClientApplication : PoqApplication() {
+    
+    private lateinit var component: TemplateClientApplicationComponent
+    
+    override fun createAppComponent(): PoqApplicationComponent {
+        component = DaggerTemplateClientApplicationComponent.builder()
+            .application(this)
+            .build()
+        return component
+    }
+    
+    override fun inject() {
+        component.inject(this)
+    }
+    
+}
